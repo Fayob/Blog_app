8ad0a8990e22f028ea2220125dd514f5a4343245
fayob <adedejifavour6@gmail.com>
2023-01-20 09:07:13 +0100

    Add post counter  method

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,4 +1,9 @@
 class Comment < ApplicationRecord
   belongs_to :author, foreign_key: :author_id, class_name: :User
   belongs_to :post
+  after_save :update_post_comments_counter
+
+  def update_post_comments_counter
+    post.increment!(:comments_counter)
+  end
 end
