ec160b8baa58dcf3aa4296d20e2f69f9d6fc4c54
fayob <adedejifavour6@gmail.com>
2023-01-20 16:28:26 +0100

    Add Validation to post model

--- app/models/post.rb
+++ app/models/post.rb
@@ -4,11 +4,18 @@ class Post < ApplicationRecord
   has_many :likes
   after_create :updates_author_posts_counter
 
+  validates :title, presence: true
+  validates :title, length: { maximum: 250 }
+  validates :comments_counter, numericality: { only_integer: true }, comparison: { greater_than_or_equal_to: 0 }
+  validates :likes_counter, numericality: { only_integer: true }, comparison: { greater_than_or_equal_to: 0 }
+
   def updates_author_posts_counter
     author.increment!(:posts_counter)
   end
 
-  def recent_five_posts
+  private :updates_author_posts_counter
+
+  def recent_five_comments
     comments.order(created_at: :desc).limit(5)
   end
 end
