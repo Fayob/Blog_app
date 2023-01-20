4d1118e626313df29c32b89f37cae64615002507
fayob <adedejifavour6@gmail.com>
2023-01-20 16:28:54 +0100

    Add validation to like model

--- app/models/like.rb
+++ app/models/like.rb
@@ -3,6 +3,8 @@ class Like < ApplicationRecord
   belongs_to :post
   after_save :update_post_likes_counter
 
+  private
+
   def update_post_likes_counter
     post.increment!(:likes_counter)
   end
