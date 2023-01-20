e883bb3d8d817e4143d9472faef3e2dd878b20ca
fayob <adedejifavour6@gmail.com>
2023-01-20 09:08:57 +0100

    Add increment and recent_five_posts method

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,3 +1,14 @@
 class Post < ApplicationRecord
-  belongs_to :author, class_name: :User, foreign_key: :author_id
+  belongs_to :author, class_name: 'User', foreign_key: :author_id
+  has_many :comments
+  has_many :likes
+  after_create :updates_author_posts_counter
+
+  def updates_author_posts_counter
+    author.increment!(:posts_counter)
+  end
+
+  def recent_five_posts
+    comments.order(created_at: :desc).limit(5)
+  end
 end
