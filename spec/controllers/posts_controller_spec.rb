96ae43842fe765a18b5ee0916bbe0607c7e31917
fayob <adedejifavour6@gmail.com>
2023-01-24 12:32:27 +0100

    Fix linters error

--- spec/controllers/posts_controller_spec.rb
+++ spec/controllers/posts_controller_spec.rb
@@ -1,9 +1,9 @@
 require './spec/rails_helper'
 
-describe PostsController, type::request do
-  describe "test for index method" do
-    before(:each) do 
-      get "/users/:id/posts"
+describe PostsController, type.request do
+  describe 'test for index method' do
+    before(:each) do
+      get '/users/:id/posts'
     end
 
     it 'should test for request status index method' do
@@ -18,10 +18,10 @@ describe PostsController, type::request do
     end
   end
 
-  describe "test for show method" do
+  describe 'test for show method' do
     before(:each) do
-      get "/users/1/posts/2"
-    end 
+      get '/users/1/posts/2'
+    end
     it 'should test for request status of show method' do
       expect(response).to have_http_status(:ok)
     end
--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -1,27 +1,27 @@
 require './spec/rails_helper'
 
-describe UsersController, type::request do
-  describe "test for index method" do
+describe UsersController, type.request do
+  describe 'test for index method' do
     it 'should test for request status of root url' do
-      get "/"
+      get '/'
       expect(response).to have_http_status(:ok)
     end
 
     it 'should render index' do
-      get "/"
+      get '/'
       expect(response).to render_template(:index)
     end
 
     it 'should test for response body' do
-      get "/"
+      get '/'
       expect(response.body).to include('Welcome')
     end
   end
 
-  describe "test for show method" do
+  describe 'test for show method' do
     before(:each) do
-      get "/users/1"
-    end 
+      get '/users/1'
+    end
     it 'should test for request status of show method' do
       expect(response).to have_http_status(:ok)
     end
