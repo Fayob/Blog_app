426628fe75ab5d74fc2e7ccc63243c1a35249f9c
fayob <adedejifavour6@gmail.com>
2023-01-26 20:03:14 +0100

    Add decrement of post functionality upon destroy

--- app/models/like.rb
+++ app/models/like.rb
@@ -1,11 +1,16 @@
 class Like < ApplicationRecord
   belongs_to :author, class_name: :User, foreign_key: :author_id
   belongs_to :post
-  after_save :update_post_likes_counter
+  after_create :increase_post_likes_counter
+  after_destroy :decrease_post_likes_counter
 
   private
 
-  def update_post_likes_counter
+  def increase_post_likes_counter
     post.increment!(:likes_counter)
+  end
+
+  def decrease_post_likes_counter
+    post.decrement!(:likes_counter)
   end
 end
