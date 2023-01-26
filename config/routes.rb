dd676326a9d922826a5db56d06046eb895072f2f
fayob <adedejifavour6@gmail.com>
2023-01-26 20:00:18 +0100

    Change and add some routes

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,12 @@ Rails.application.routes.draw do
   root "users#index"
   get "/users/:id", to: "users#show"
   get "/users/:user_id/posts", to: "posts#index"
+  get "/users/:user_id/all_posts", to: "posts#all_posts"
+  get "/users/:author_id/posts/new", to: "posts#new"
+  post "/users/:author_id/posts/create", to: "posts#create"
   get "/users/:user_id/posts/:id", to: "posts#show"
+  post "/users/:user_id/posts/:post_id/comments", to: "comments#create"
+  post "/users/:user_id/posts/:post_id/likes", to: "likes#create"
 
   # Defines the root path route ("/")
   # root "articles#index"
