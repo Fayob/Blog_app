38dc9e24cf63921915304f5d33c669e0b6f48f4b
fayob <adedejifavour6@gmail.com>
2023-01-27 09:30:26 +0100

    make changes

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,6 +1,6 @@
 class CommentsController < ApplicationController
   def create
-    @user = User.find(params[:user_id])
+    @user = current_user
     @post = Post.find(params[:post_id])
     param = params.require(:comment).permit(:text)
     @comment = Comment.new(author: @user, post: @post, text: param[:text])
