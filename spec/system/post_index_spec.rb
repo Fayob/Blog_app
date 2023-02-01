651c953e6823aeb9be4c62a22a48c1a2a55bf446
fayob <adedejifavour6@gmail.com>
2023-02-01 13:00:59 +0100

    Fix linters error

--- spec/system/post_index_spec.rb
+++ spec/system/post_index_spec.rb
@@ -10,73 +10,72 @@ describe 'Posts Index Page', type: :system do
     @posts = Post.where(author_id: @user.id).includes(:comments)
   end
 
-    it "shows the user's profile photo" do
-      visit ("/users/#{@user.id}/posts")
+  it "shows the user's profile photo" do
+    visit("/users/#{@user.id}/posts")
 
-      expect(page).to have_css("img[src*=\"#{@user.photo}\"]")
+    expect(page).to have_css("img[src*=\"#{@user.photo}\"]")
+  end
+
+  it "shows the user's name" do
+    visit("/users/#{@user.id}/posts")
+
+    expect(page).to have_text(@user.name)
+  end
+
+  it "shows the user's number of posts" do
+    visit("/users/#{@user.id}/posts")
+
+    expect(page).to have_content(@user.posts_counter)
+  end
+
+  it "shows post's title of each post" do
+    visit("/users/#{@user.id}/posts")
+
+    @posts.each do |post|
+      expect(page).to have_text(post.title)
     end
+  end
 
-    it "shows the user's name" do
-      visit ("/users/#{@user.id}/posts")
+  it "shows post's description of each post" do
+    visit("/users/#{@user.id}/posts")
 
-      expect(page).to have_text(@user.name)
+    @posts.each do |post|
+      expect(page).to have_content(post.text)
     end
+  end
 
-    it "shows the user's number of posts" do
-      visit ("/users/#{@user.id}/posts")
+  it 'shows number of comments for each post' do
+    visit("/users/#{@user.id}/posts")
 
-      expect(page).to have_content(@user.posts_counter)
+    @posts.each do |post|
+      expect(page).to have_text(post.comments_counter)
     end
+  end
 
-    it "shows post's title of each post" do
-      visit ("/users/#{@user.id}/posts")
+  it 'shows number of likes for each post' do
+    visit("/users/#{@user.id}/posts")
 
-      @posts.each do |post|
-        expect(page).to have_text(post.title)
+    @posts.each do |post|
+      expect(page).to have_text(post.likes_counter)
+    end
+  end
+
+  it 'shows the first comments on a post' do
+    visit("/users/#{@user.id}/posts")
+
+    @posts.each do |post|
+      if post.comments_counter > 0
+        comment = post.comments.first
+        expect(page).to have_content(comment.text)
       end
     end
+  end
 
-    it "shows post's description of each post" do
-      visit ("/users/#{@user.id}/posts")
+  it 'redirects to post show page when click on a post' do
+    visit("/users/#{@user.id}/posts")
+    post = @posts.first
+    click_on 'Show this post', match: :first
 
-      @posts.each do |post|
-        expect(page).to have_content(post.text)
-      end
-    end
-
-    it 'shows number of comments for each post' do
-      visit ("/users/#{@user.id}/posts")
-
-      @posts.each do |post|
-        expect(page).to have_text(post.comments_counter)
-      end
-    end
-
-    it 'shows number of likes for each post' do
-      visit ("/users/#{@user.id}/posts")
-
-      @posts.each do |post|
-        expect(page).to have_text(post.likes_counter)
-      end
-    end
-
-    it 'shows the first comments on a post' do
-      visit ("/users/#{@user.id}/posts")
-
-      @posts.each do |post|
-        if post.comments_counter > 0
-          comment = post.comments.first
-          expect(page).to have_content(comment.text)
-        end
-      end
-    end
-
-    it "redirects to post show page when click on a post" do
-      visit("/users/#{@user.id}/posts")
-      post = @posts.first
-      click_on 'Show this post', match: :first
-
-      expect(page).to have_current_path "/users/#{@user.id}/posts/#{post.id}"
-
-    end
+    expect(page).to have_current_path "/users/#{@user.id}/posts/#{post.id}"
+  end
 end
