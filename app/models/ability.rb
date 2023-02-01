fa374aeae16504ccd33bda46ab605748046f4c18
fayob <adedejifavour6@gmail.com>
2023-02-01 18:51:22 +0100

    Add role to db

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -4,6 +4,16 @@ class Ability
   include CanCan::Ability
 
   def initialize(user)
+    
+    can :read, Post, public: true  # start by defining rules for all users, also not logged ones
+    return unless user.present?
+    can :manage, Post, user_id: user.id # if the user is logged in can manage it's own posts
+    can :create, Comment # logged in users can also create comments
+    # return unless user.manager? # if the user is a manager we give additional permissions
+    # can :manage, Comment # like managing all comments in the website
+    return unless user.admin?
+    can :manage, :all # finally we give all remaining permissions only to the admins
+
     # Define abilities for the user here. For example:
     #
     #   return unless user.present?
