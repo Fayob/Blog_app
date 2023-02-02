a2fcca31e0229c5285a3d4a82e2cc19e33d47201
fayob <adedejifavour6@gmail.com>
2023-02-02 13:48:18 +0100

    Add destroy method to comments controller

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -7,4 +7,12 @@ class CommentsController < ApplicationController
     @comment.save!
     redirect_to "/users/#{@user.id}/posts/#{@post.id}"
   end
+
+  def destroy
+    @user = User.find(params[:user_id])
+    @post = params[:post_id]
+    @comment = Comment.where(author: @user, post_id: @post, id: params[:id])
+    @comment.destroy(@comment.ids)
+    redirect_to "/users/#{@user.id}/posts/#{@post}"
+  end
 end
