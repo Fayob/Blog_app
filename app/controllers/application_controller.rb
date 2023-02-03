d8730bf3dd6c443144938681851960884d1d2a9d
fayob <adedejifavour6@gmail.com>
2023-02-03 14:31:39 +0100

    Add authentication before any action

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,15 +1,31 @@
 class ApplicationController < ActionController::API
   # protect_from_forgery with: :exception
-  attr_reader :current_user
+  before_action :configure_permitted_parameters, if: :devise_controller?
+  include JsonWebToken
+
+  before_action :authenticate_request
 
   respond_to :json
   
-  before_action :configure_permitted_parameters, if: :devise_controller?
+
+  # def encode_token(payload)
+  #   JWT.encode(payload, 'secret')
+  # end
+
+  private
+
+  def authenticate_request
+    header = request.headers["Authorization"]
+    header = header.split(" ").last if header
+    decoded = jwt_decode(header)
+    @current_user = User.find(decoded[:user_id])
+  end
 
   protected
 
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up) { |u| u.permit(:name, :email, :password) }
+    devise_parameter_sanitizer.permit(:sign_in) { |u| u.permit(:email, :encrypted_password) }
 
     devise_parameter_sanitizer.permit(:account_update) do |u|
       u.permit(:name, :email, :password, :current_password)
